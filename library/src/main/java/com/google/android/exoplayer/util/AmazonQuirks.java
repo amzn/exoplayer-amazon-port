87c10d7c3b27ee5afe0eb4faf1d961362d1f14cf
Srikanth Peddibhotla <peddisri@lab126.com>
2015-04-21 16:57:32 -0700

    FTVPI-4: FTVPI-611: FTVPI-670: Fix for handling audio latencies inconsistency in FireOS4 devices Workaround to fix infinite buffering after seek in FOS4 Tablets Fix for exception in JB based devices
    First Commit message:
    
    [Problem]
    1. Video playback starts, but stops immediately on FireOS 4 tablets.
    2. Sometimes on seeking, playback stops on FireOS 4 tablets.
    3. In FOS4 tablets, after seek, the playback goes into buffering state and never resumes playback.
    
    [Root cause]
    In QCOM and MTK based Amazon devices running KitKat,
    the AudioTrack (specifically getPlaybackHeadPosition implementation) has several issues.
    
    AudioTrack accounts for audio latencies in the head position returned by
    getPlaybackHeadPosition API.
    Due to this, during the latency  stablization phase that happens at start of audio
    playbak, it no longer returns a continuously advancing frame position counter, instead
    can dip a bit till steady state is reached.
    This happens at the start of playback as well as after every seek.
    
    Additionally,this API pre-deducts software latencies from the head position.
    Exoplayer is dependent on both getTimeStamp and getPlaybackHeadPosition APIs and
    it does not expect latencies to be pre-deducted from the returned value.
    Instead, ExoPlayer calls getLatency java API of AudioTrack to compute hardware latencies
    and deducts it from the current audio position.
    
    MediaController sometimes issues duplicate seek calls to the media player
    on touching the progress bar once.
    On a seek operation, ExoPlayer pauses the audio track.
    When such duplicate quick seeks are issued to exoPlayer, the sequence of calls
    to audio track are pause, play and pause in quick succession.
    In such a scenario Audio track does not pause rendering audio immediately
    and continues to increment the playback head position.
    
    But since pause was issued immediately after a play, the audio track has not yet
    stablized the latency calculation and after few iterations we see a dip
    in the playback head position.
    
    Moreover, sometimes, even after adding latency compenstation during the stablization
    phase, the compensated playback head position is a negative number - in terms of playback
    head position a very large number because the returned value even though it is an int
    is considered as an unsigned integer value as per Android documentation.
    http://developer.android.com/reference/android/media/AudioTrack.html#getPlaybackHeadPosition()
    
    These issues results in large jump in calculated play head position and audio current position.
    
    [Solution]
    Add back the software latencies to getPlaybackHeadPosition returned values in AudioTrack
    module of ExoPlayer when either in playing state or when in paused state and current play
    head position valie is non-zero.
    
    Improve playback head position wrap around detection logic to handle the dips.
    
    Set the HW playback latencies to be around 90 ms that matches the latencies
    for all amazon based tablet devices.
    
    If within 1 second of start of playback, the play head position is negative,
    reset it to 0.
    
    [Tests] Tested video playback, seek and audio/video sync in
    Fire Tablets & phone.
    
    Change-Id: I42e75d9abae182909f1fc39a9e1aa4b6e9d97c10
    Reviewed-on: https://e-gerrit.labcollab.net/40590
    Reviewed-by: Zsolt Matyas <zmmatyas@amazon.com>
    Tested-by: Zsolt Matyas <zmmatyas@amazon.com>
    Reviewed-on: https://e-gerrit.labcollab.net/41348
    Reviewed-by: Tarun Solanki <tarunsol@lab126.com>
    Reviewed-by: Srikanth Peddibhotla <peddisri@lab126.com>
    Tested-by: Srikanth Peddibhotla <peddisri@lab126.com>
    Reviewed-on: https://e-gerrit.labcollab.net/43182

---
+++ library/src/main/java/com/google/android/exoplayer/util/AmazonQuirks.java
@@ -0,0 +1,87 @@
+/*
+ * Copyright (C) 2014 The Android Open Source Project
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package com.google.android.exoplayer.util;
+
+import android.os.Build;
+import android.util.Log;
+
+ public class AmazonQuirks {
+     //ordering of the static initializations is important.
+     private static final String TAG = AmazonQuirks.class.getSimpleName();
+     private static final String FIRETV_GEN1_DEVICE_MODEL = "AFTB";
+     private static final String FIRETV_GEN2_DEVICE_MODEL = "AFTS";
+     private static final String FIRETV_STICK_DEVICE_MODEL = "AFTM";
+     private static final String KINDLE_TABLET_DEVICE_MODEL = "KF";
+     private static final String AMAZON = "Amazon";
+     private static final String DEVICEMODEL = Build.MODEL;
+     private static final String MANUFACTURER = Build.MANUFACTURER;
+     private static final int AUDIO_HARDWARE_LATENCY_FOR_TABLETS = 90000;
+     //caching
+     private static final boolean isAmazonDevice;
+     private static final boolean isFireTVGen1;
+     private static final boolean isFireTVStick;
+     private static final boolean isFireTVGen2;
+     private static final boolean isKindleTablet;
+     private static final long fireTVFireOsBuildVersion;
+
+     static {// This static block must be at the end
+         //Init ordering is important within this block
+         isAmazonDevice = MANUFACTURER.equalsIgnoreCase(AMAZON);
+         isFireTVGen1 = isAmazonDevice && DEVICEMODEL.equalsIgnoreCase(FIRETV_GEN1_DEVICE_MODEL);
+         isFireTVGen2 = isAmazonDevice && DEVICEMODEL.equalsIgnoreCase(FIRETV_GEN2_DEVICE_MODEL);
+         isFireTVStick = isAmazonDevice && DEVICEMODEL.equalsIgnoreCase(FIRETV_STICK_DEVICE_MODEL);
+         isKindleTablet = isAmazonDevice && DEVICEMODEL.startsWith(KINDLE_TABLET_DEVICE_MODEL);
+         fireTVFireOsBuildVersion = getBuildVersion();
+     }
+
+     private AmazonQuirks(){}
+
+     public static boolean isLatencyQuirkEnabled() {
+         // Sets latency quirk for Amazon KK and JB Tablets
+         return ( (Util.SDK_INT <= 19) && isKindleTablet );
+     }
+
+     public static int getAudioHWLatency() {
+         // this function is called only when the above function
+         // returns true for latency quirk. So no need to check for
+         // SDK version and device type again
+         return AUDIO_HARDWARE_LATENCY_FOR_TABLETS;
+     }
+
+     public static boolean isAmazonDevice(){
+         return isAmazonDevice;
+     }
+
+     public static boolean isFireTVGen1Family() {
+         return isFireTVGen1 || isFireTVStick;
+     }
+
+     public static boolean isFireTVGen2() {
+         return isFireTVGen2;
+     }
+
+     private static long getBuildVersion() {
+         try {
+             String[] verSplit = Build.VERSION.INCREMENTAL.split("_");
+             if (verSplit.length > 2) {
+                 return Long.valueOf(verSplit[2]);
+             }
+         } catch (Exception e) {
+             Log.e(TAG,"Exception in finding build version",e);
+         }
+         return Long.MAX_VALUE;
+     }
+ }
