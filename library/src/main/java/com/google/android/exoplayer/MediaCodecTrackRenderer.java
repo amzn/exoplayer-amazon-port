a79d0c48069bada1776977d9c19abb88e6c85fd5
Tarun Solanki <tarunsol@lab126.com>
2015-07-23 17:30:43 -0700

    FTVPI-200: Expose the codec counters of MediaCodecTrackRenderer allowing queries about renderer frame/codec metrics. [Patch 1/2]
    This will expose the CodecCounters for Audio and Video media codec track renderer
    to be called from AMZNMediaPlayer
    
    Change-Id: I16f192dd8ea23dc7e86602e7224cc28002eed618
    Reviewed-on: https://e-gerrit.labcollab.net/33685
    Reviewed-by: Zsolt Matyas <zmmatyas@amazon.com>
    Reviewed-by: Srikanth Peddibhotla <peddisri@lab126.com>
    Reviewed-by: Tarun Solanki <tarunsol@lab126.com>
    Tested-by: Tarun Solanki <tarunsol@lab126.com>

--- library/src/main/java/com/google/android/exoplayer/MediaCodecTrackRenderer.java
+++ library/src/main/java/com/google/android/exoplayer/MediaCodecTrackRenderer.java
@@ -986,5 +986,28 @@ public abstract class MediaCodecTrackRenderer extends TrackRenderer {
     }
     return -1;
   }
-
+  //AMZN_CHANGE_BEGIN
+  //The specific media renderer subclasses (Audio/Video) will update these values.
+  public int getRenderedOutputBufferCount() {
+    return codecCounters.renderedOutputBufferCount;
+  }
+  public int getDroppedOutputBufferCount() {
+    return codecCounters.droppedOutputBufferCount;
+  }
+  public int getSkippedOutputBufferCount() {
+    return codecCounters.skippedOutputBufferCount;
+  }
+  public int getOutputBuffersChangedCount() {
+    return codecCounters.outputBuffersChangedCount;
+  }
+  public int getOutputFormatChangedCount() {
+    return codecCounters.outputFormatChangedCount;
+  }
+  public int getCodecReleaseCount() {
+    return codecCounters.codecReleaseCount;
+  }
+  public int getCodecInitCount() {
+    return codecCounters.codecInitCount;
+  }
+  //AMZN_CHANGE_END
 }
