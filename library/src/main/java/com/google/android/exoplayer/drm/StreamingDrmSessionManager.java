8f91a9c43b261f1989d79d3fb838ce440aae5175
Don Darling <darling@lab126.com>
2015-10-16 21:31:17 -0700

    FTVPI-464: Playready Encrypted content doesn't play after upgrading to ExoPlayer 1.4.2
    Extract PlayReady header from PSSH for MediaDrm
    
    [Problem]
    In ExoPlayer 1.4.1, a change was introduced that sends the entire PSSH box
    to MediaDrm instead of just the PlayReady header.  This was done to fix an
    issue with SmoothStreaming on Android TV.
    
    However, PlayReady support on Amazon Fire TV devices currently expects the
    PlayReady header only -- not the entire PSSH box.  Modifying Amazon devices
    to handle the entire PSSH box would require changes to three different
    PlayReady implementations, so for now it is better to simply restore
    ExoPlayer behavior to pre-1.4.1 behavior.
    
    [Solution]
    For Amazon devices, extract the PlayReady header from the PSSH box prior to
    sending it to MediaDrm.
    
    [Test]
    - Verified playback works with Playready smoothstreaming content on Bueller
      1.6.3 and Montoya 1.2.1.
    - Verified MediaDrm.getKeyRequest() succeeds on FireOS 5 with Montoya,
      Bueller, and Sloane (other codec initialization issues still exist, and
      will be addressed later).
    
    Change-Id: I5b22087ba8d3c687be85acea851d4c976ab6f5b8
    Reviewed-on: https://e-gerrit.labcollab.net/36564
    Tested-by: Don Darling <darling@lab126.com>
    Reviewed-by: Srikanth Peddibhotla <peddisri@lab126.com>
    Reviewed-by: Don Darling <darling@lab126.com>

--- library/src/main/java/com/google/android/exoplayer/drm/StreamingDrmSessionManager.java
+++ library/src/main/java/com/google/android/exoplayer/drm/StreamingDrmSessionManager.java
@@ -17,6 +17,7 @@ package com.google.android.exoplayer.drm;
 
 import com.google.android.exoplayer.extractor.mp4.PsshAtomUtil;
 import com.google.android.exoplayer.util.Util;
+import com.google.android.exoplayer.util.AmazonQuirks; // AMZN_CHANGE_ONELINE
 
 import android.annotation.SuppressLint;
 import android.annotation.TargetApi;
@@ -280,15 +281,30 @@ public class StreamingDrmSessionManager implements DrmSessionManager {
         onError(new IllegalStateException("Media does not support uuid: " + uuid));
         return;
       }
-      if (Util.SDK_INT < 21) {
-        // Prior to L the Widevine CDM required data to be extracted from the PSSH atom.
-        byte[] psshData = PsshAtomUtil.parseSchemeSpecificData(schemeData, WIDEVINE_UUID);
+
+      // AMZN_CHANGE_BEGIN
+      boolean extractSchemeSpecificData = false;
+      if (Util.SDK_INT < 21 && uuid.equals(WIDEVINE_UUID)) {
+        // Prior to L the Widevine CDM required data to be extracted from the
+        // PSSH atom.
+        extractSchemeSpecificData = true;
+      }
+
+      if (AmazonQuirks.isFireTVFamily() && uuid.equals(PLAYREADY_UUID)) {
+        // PlayReady support on Amazon Fire TV devices require the PlayReady
+        // header to be extracted from the PSSH
+        extractSchemeSpecificData = true;
+      }
+
+      if (extractSchemeSpecificData == true) {
+        byte[] psshData = PsshAtomUtil.parseSchemeSpecificData(schemeData, uuid);
         if (psshData == null) {
-          // Extraction failed. schemeData isn't a Widevine PSSH atom, so leave it unchanged.
+          // Extraction failed; leave it unchanged.
         } else {
           schemeData = psshData;
         }
       }
+      // AMZN_CHANGE_END
     }
     state = STATE_OPENING;
     openInternal(true);
