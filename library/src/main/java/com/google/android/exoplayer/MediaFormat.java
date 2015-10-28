d32fe045d392ff0bf519971baa2adadfddb4234d
Srikanth Peddibhotla <peddisri@lab126.com>
2015-10-28 16:47:42 -0700

    FTVPI-568: Workaround to avoid sending CSD data in Configure API of Video decoder
    [Problem]
    Video decoder configuration failed in playback of Playready encrypted content
    
    [Root cause]
    FireTV Gen2 has a platform limitation that it cannot handle clear data sent
    in secure buffer and this happens when the application sends CSD data in clear
    as part of MediaFormat  and also provides a valid non-null
    crypto object in configure API.
    
    [Woraround]
    Till this issue is fixed in platform, we need to avoid sending CSD
    data as part of MediaFormat in configure API for video decoder only.
    Instead this should be sent in clear as part of first frame which is already
    being done as part of https://e-gerrit.labcollab.net/#/c/36872/
    
    Platform fix is internally tracked by SLAMFOSV-5597
    
    [Target Devices]
    Fire TV Gen2 only.
    
    [Tests]
    Tested encrypted and clear content playback on Fire TV Gen2.
    
    [Notes] This workaround will work even after the platform fix.
    This workaround applies only for Fire TV Gen2
    
    Change-Id: Ib90cfa1f620722ea8d153bd2e352af47235c0a0f
    Reviewed-on: https://e-gerrit.labcollab.net/36866
    Reviewed-by: Tarun Solanki <tarunsol@lab126.com>
    Reviewed-by: Srikanth Peddibhotla <peddisri@lab126.com>
    Tested-by: Srikanth Peddibhotla <peddisri@lab126.com>
    Reviewed-on: https://e-gerrit.labcollab.net/36873
    Reviewed-by: Zsolt Matyas <zmmatyas@amazon.com>
    Reviewed-on: https://e-gerrit.labcollab.net/37766
    Tested-by: Zsolt Matyas <zmmatyas@amazon.com>

--- library/src/main/java/com/google/android/exoplayer/MediaFormat.java
+++ library/src/main/java/com/google/android/exoplayer/MediaFormat.java
@@ -17,7 +17,10 @@ package com.google.android.exoplayer;
 
 import com.google.android.exoplayer.util.Assertions;
 import com.google.android.exoplayer.util.Util;
-
+// AMZN_CHANGE_BEGIN
+import com.google.android.exoplayer.util.MimeTypes;
+import com.google.android.exoplayer.util.AmazonQuirks;
+//AMZN_CHANGE_END
 import android.annotation.SuppressLint;
 import android.annotation.TargetApi;
 
@@ -30,7 +33,6 @@ import java.util.List;
  * Defines the format of an elementary media stream.
  */
 public final class MediaFormat {
-
   public static final int NO_VALUE = -1;
 
   /**
@@ -252,9 +254,15 @@ public final class MediaFormat {
       maybeSetIntegerV16(format, android.media.MediaFormat.KEY_MAX_HEIGHT, maxHeight);
       maybeSetIntegerV16(format, android.media.MediaFormat.KEY_CHANNEL_COUNT, channelCount);
       maybeSetIntegerV16(format, android.media.MediaFormat.KEY_SAMPLE_RATE, sampleRate);
-      for (int i = 0; i < initializationData.size(); i++) {
-        format.setByteBuffer("csd-" + i, ByteBuffer.wrap(initializationData.get(i)));
+      // AMZN_CHANGE_BEGIN
+      if ( !(AmazonQuirks.isFireTVGen2() && MimeTypes.isVideo(mimeType)) ){
+        for (int i = 0; i < initializationData.size(); i++) {
+          format.setByteBuffer("csd-" + i, ByteBuffer.wrap(initializationData.get(i)));
+        }
+      } else {
+        //log.i("Not sending Video CSD in configure for Amazon Fire TV Gen2!!!");
       }
+      // AMZN_CHANGE_END
       if (durationUs != C.UNKNOWN_TIME_US) {
         format.setLong(android.media.MediaFormat.KEY_DURATION, durationUs);
       }
