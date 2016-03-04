amzn/exoplayer-amazon-port

commit fcb0e4e3e206de50d9c48d88f7a4dbb228ae4019

FTVPI-568: FTVPI-800: Workaround to avoid sending CSD data in Configure API of Video decoder Fix for exception at the end of Playready content playback in Fire TV Gen2
[Problem]
Video decoder configuration failed in playback of Playready encrypted content

[Root cause]
FireTV Gen2 has a platform limitation that it cannot handle clear data sent
in secure buffer and this happens when the application sends CSD data in clear
as part of MediaFormat  and also provides a valid non-null
crypto object in configure API.

[Woraround]
Till this issue is fixed in platform, we need to avoid sending CSD
data as part of MediaFormat in configure API for video decoder only.
Instead this should be sent in clear as part of first frame which is already
being done as part of "FTVPI-34: Fix for Codec error VIDC_1080P_ERROR_HEADER_NOT_FOUND"

Platform fix is internally tracked by SLAMFOSV-5597

[Target Devices]
Fire TV Gen2 only.

[Tests]
Tested encrypted and clear content playback on Fire TV Gen2.

[Notes] This workaround will work even after the platform fix.
This workaround applies only for Fire TV Gen2

[Problem]
When the PlayReady encrypted playback ends and the player sends an empty frame to MediaCodec
with  BUFFER_FLAG_END_OF_STREAM flag set, MediaCodec throws an exception.

[Root cause]
FireTV Gen2 has a platform limitation that it cannot handle empty clear buffer in
queueInputBuffer with BUFFER_FLAG_END_OF_STREAM flag set if the MediaCodec is
associated with a valid Crypto object.

[Solution]
Since this issue is fixed in future OS version as per SLAMFOSV-5597,
enable the codecNeedsEosPropagationWorkaround workaround for Fire TV Gen2 for
the OS versions with this issue. This workaround avoids sending empty frame
with BUFFER_FLAG_END_OF_STREAM flag, and handles the end of stream detection
in the player differently.

Change-Id: Ib90cfa1f620722ea8d153bd2e352af47235c0a0f
Reviewed-on: https://e-gerrit.labcollab.net/38324
Reviewed-by: Srikanth Peddibhotla <peddisri@lab126.com>

library/src/main/java/com/google/android/exoplayer/MediaFormat.java
@@ -18,7 +18,9 @@ package com.google.android.exoplayer;
 import com.google.android.exoplayer.util.Assertions;
 import com.google.android.exoplayer.util.MimeTypes;
 import com.google.android.exoplayer.util.Util;
-
+// AMZN_CHANGE_BEGIN
+import com.google.android.exoplayer.util.AmazonQuirks;
+//AMZN_CHANGE_END
 import android.annotation.SuppressLint;
 import android.annotation.TargetApi;
 
@@ -31,7 +33,6 @@ import java.util.List;
  * Defines the format of an elementary media stream.
  */
 public final class MediaFormat {
-
   public static final int NO_VALUE = -1;
 
   /**
@@ -293,6 +294,15 @@ public final class MediaFormat {
       for (int i = 0; i < initializationData.size(); i++) {
         format.setByteBuffer("csd-" + i, ByteBuffer.wrap(initializationData.get(i)));
       }
+      // AMZN_CHANGE_BEGIN
+      if ( AmazonQuirks.shouldSkipCSDInConfigure(mimeType) ){
+        //log.i("Not sending Video CSD in configure for Amazon Fire TV Gen2!!!");
+      } else {
+        for (int i = 0; i < initializationData.size(); i++) {
+          format.setByteBuffer("csd-" + i, ByteBuffer.wrap(initializationData.get(i)));
+        }
+      }
+      // AMZN_CHANGE_END
       if (durationUs != C.UNKNOWN_TIME_US) {
         format.setLong(android.media.MediaFormat.KEY_DURATION, durationUs);
       }
@@ -395,5 +405,4 @@ public final class MediaFormat {
       format.setInteger(key, value);
     }
   }
-
 }

library/src/main/java/com/google/android/exoplayer/MediaCodecTrackRenderer.java
@@ -18,6 +18,7 @@ package com.google.android.exoplayer;
 import com.google.android.exoplayer.MediaCodecUtil.DecoderQueryException;
 import com.google.android.exoplayer.drm.DrmInitData;
 import com.google.android.exoplayer.drm.DrmSessionManager;
+import com.google.android.exoplayer.util.AmazonQuirks;
 import com.google.android.exoplayer.util.Assertions;
 import com.google.android.exoplayer.util.NalUnitUtil;
 import com.google.android.exoplayer.util.TraceUtil;
@@ -1045,7 +1046,8 @@ public abstract class MediaCodecTrackRenderer extends SampleSourceTrackRenderer 
    *     propagation incorrectly on the host device. False otherwise.
    */
   private static boolean codecNeedsEosPropagationWorkaround(String name) {
-    return Util.SDK_INT <= 17 && "OMX.rk.video_decoder.avc".equals(name);
+    return AmazonQuirks.codecNeedsEosPropagationWorkaround(name) ||
+             ( Util.SDK_INT <= 17 && "OMX.rk.video_decoder.avc".equals(name) ); // AMZN_CHANGE_ONELINE
   }
 
   /**

library/src/main/java/com/google/android/exoplayer/util/AmazonQuirks.java
@@ -31,6 +31,23 @@ import android.util.Log;
   private static final String DEVICEMODEL = Build.MODEL;
   private static final String MANUFACTURER = Build.MANUFACTURER;
   private static final int AUDIO_HARDWARE_LATENCY_FOR_TABLETS = 90000;
+  private static final long FIRETV_GEN2_FOS5_PR_CLEAR_FIX_OS_BUILD_NUM = 550078110;
+
+
+  private static final long fireTVFireOsBuildVersion = getBuildVersion();
+
+  private static long getBuildVersion() {
+    try {
+      String[] verSplit = Build.VERSION.INCREMENTAL.split("_");
+      if (verSplit.length > 2) {
+        return Long.valueOf(verSplit[2]);
+      }
+    } catch (Exception e) {
+       Log.e(TAG,"Exception in finding build version",e);
+    }
+    return Long.MAX_VALUE;
+  }
+
 
   public static boolean isAdaptive(String mimeType) {
     if (mimeType == null || mimeType.isEmpty()) {
@@ -96,4 +113,16 @@ import android.util.Log;
     Log.i(TAG,"using platform Dolby decoder");
     return false;
   }
- }
+
+
+  public static boolean codecNeedsEosPropagationWorkaround(String name) {
+    return (isFireTVGen2() && fireTVFireOsBuildVersion <= FIRETV_GEN2_FOS5_PR_CLEAR_FIX_OS_BUILD_NUM);
+  }
+
+  public static boolean shouldSkipCSDInConfigure(String mimeType) {
+    return isFireTVGen2()
+         && fireTVFireOsBuildVersion <= FIRETV_GEN2_FOS5_PR_CLEAR_FIX_OS_BUILD_NUM
+         && MimeTypes.isVideo(mimeType);
+
+  }
+}

commit 4449b97e0e4e81da91f78338cb5882f1bf2306f7

Regression in commit fcb0e4e3 fixed

library/src/main/java/com/google/android/exoplayer/MediaFormat.java
@@ -291,9 +291,6 @@ public final class MediaFormat {
       maybeSetIntegerV16(format, android.media.MediaFormat.KEY_SAMPLE_RATE, sampleRate);
       maybeSetIntegerV16(format, "encoder-delay", encoderDelay);
       maybeSetIntegerV16(format, "encoder-padding", encoderPadding);
-      for (int i = 0; i < initializationData.size(); i++) {
-        format.setByteBuffer("csd-" + i, ByteBuffer.wrap(initializationData.get(i)));
-      }
       // AMZN_CHANGE_BEGIN
       if ( AmazonQuirks.shouldSkipCSDInConfigure(mimeType) ){
         //log.i("Not sending Video CSD in configure for Amazon Fire TV Gen2!!!");