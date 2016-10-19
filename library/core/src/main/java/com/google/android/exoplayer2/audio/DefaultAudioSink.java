663c262994cfa6629e7f78045dce89395017e3b3
Zsolt Matyas <zmmatyas@amazon.com>
2016-10-19 09:55:30 -0700

    FTVPI-1552: Gen4 Tablet audio latency issues
    Original issues that are merged with this commit:
    FTVPI-4: FTVPI-611: FTVPI-670: Fix for handling audio latencies
    inconsistency in FireOS4 devices
    
    Workaround to fix infinite buffering after seek in FOS4 Tablets
    Fix for exception in JB based devices
    
    [Problem]
    1. Video playback starts, but stops immediately on FireOS 4 tablets.
    2. Sometimes on seeking, playback stops on FireOS 4 tablets.
    3. In FOS4 tablets, after seek, the playback goes into buffering state and never
       resumes playback.
    
    [Root cause]
    In QCOM and MTK based Amazon devices running KitKat,
    the AudioTrack (specifically getPlaybackHeadPosition implementation) has several issues.
    
    AudioTrack accounts for audio latencies in the head position returned by
    getPlaybackHeadPosition API.
    Due to this, during the latency  stablization phase that happens at start of audio
    playbak, it no longer returns a continuously advancing frame position counter, instead
    can dip a bit till steady state is reached.
    This happens at the start of playback as well as after every seek.
    
    Additionally,this API pre-deducts software latencies from the head position.
    Exoplayer is dependent on both getTimeStamp and getPlaybackHeadPosition APIs and
    it does not expect latencies to be pre-deducted from the returned value.
    Instead, ExoPlayer calls getLatency java API of AudioTrack to compute hardware latencies
    and deducts it from the current audio position.
    
    MediaController sometimes issues duplicate seek calls to the media player
    on touching the progress bar once.
    On a seek operation, ExoPlayer pauses the audio track.
    When such duplicate quick seeks are issued to exoPlayer, the sequence of calls
    to audio track are pause, play and pause in quick succession.
    In such a scenario Audio track does not pause rendering audio immediately
    and continues to increment the playback head position.
    
    But since pause was issued immediately after a play, the audio track has not yet
    stablized the latency calculation and after few iterations we see a dip
    in the playback head position.
    
    Moreover, sometimes, even after adding latency compenstation during the stablization
    phase, the compensated playback head position is a negative number - in terms of playback
    head position a very large number because the returned value even though it is an int
    is considered as an unsigned integer value as per Android documentation.
    http://developer.android.com/reference/android/media/AudioTrack.html#getPlaybackHeadPosition()
    
    These issues results in large jump in calculated play head position and audio current position.
    
    [Solution]
    Add back the software latencies to getPlaybackHeadPosition returned values in AudioTrack
    module of ExoPlayer when either in playing state or when in paused state and current play
    head position valie is non-zero.
    
    Improve playback head position wrap around detection logic to handle the dips.
    
    Set the HW playback latencies to be around 90 ms that matches the latencies
    for all amazon based tablet devices.
    
    If within 1 second of start of playback, the play head position is negative,
    reset it to 0.
    
    [Tests] Tested video playback, seek and audio/video sync in
    Fire Tablets & phone.
    
    Change-Id: I2f2499f86ddd4716c913d4a882a341aa8878c800
    
    cr https://code.amazon.com/reviews/CR-1572323

--- library/core/src/main/java/com/google/android/exoplayer2/audio/DefaultAudioSink.java
+++ library/core/src/main/java/com/google/android/exoplayer2/audio/DefaultAudioSink.java
@@ -286,6 +286,7 @@ public final class DefaultAudioSink implements AudioSink {
   private boolean tunneling;
   private long lastFeedElapsedRealtimeMs;
   // AMZN_CHANGE_BEGIN
+  private static final boolean isLatencyQuirkEnabled = AmazonQuirks.isLatencyQuirkEnabled();
   private final boolean isLegacyPassthroughQuirckEnabled = AmazonQuirks.isDolbyPassthroughQuirkEnabled();
   // AMZN_CHANGE_END
 
@@ -345,7 +346,8 @@ public final class DefaultAudioSink implements AudioSink {
     this.audioProcessorChain = Assertions.checkNotNull(audioProcessorChain);
     this.enableConvertHighResIntPcmToFloat = enableConvertHighResIntPcmToFloat;
     releasingConditionVariable = new ConditionVariable(true);
-    audioTrackPositionTracker = new AudioTrackPositionTracker(new PositionTrackerListener());
+    audioTrackPositionTracker = new AudioTrackPositionTracker(new PositionTrackerListener(),
+                                      isLatencyQuirkEnabled); // AMZN_CHANGE_ONELINE
     channelMappingAudioProcessor = new ChannelMappingAudioProcessor();
     trimmingAudioProcessor = new TrimmingAudioProcessor();
     ArrayList<AudioProcessor> toIntPcmAudioProcessors = new ArrayList<>();
@@ -358,6 +360,12 @@ public final class DefaultAudioSink implements AudioSink {
     toIntPcmAvailableAudioProcessors =
         toIntPcmAudioProcessors.toArray(new AudioProcessor[toIntPcmAudioProcessors.size()]);
     toFloatPcmAvailableAudioProcessors = new AudioProcessor[] {new FloatResamplingAudioProcessor()};
+    // AMZN_CHANGE_BEGIN
+    Log.i(TAG,"Amazon quirks:"
+            + " Latency:" + (isLatencyQuirkEnabled ? "on" : "off")
+            + "; Dolby" + (isLegacyPassthroughQuirckEnabled ? "on" : "off")
+            + ". On Sdk: " + Util.SDK_INT);
+    // AMZN_CHANGE_END
     volume = 1.0f;
     startMediaTimeState = START_NOT_SET;
     audioAttributes = AudioAttributes.DEFAULT;
@@ -1094,6 +1102,7 @@ public final class DefaultAudioSink implements AudioSink {
             positionUs - playbackParametersPositionUs, playbackParameters.speed);
   }
 
+
   private long applySkipping(long positionUs) {
     return positionUs + framesToDurationUs(audioProcessorChain.getSkippedOutputFrameCount());
   }
@@ -1367,7 +1376,6 @@ public final class DefaultAudioSink implements AudioSink {
       this.mediaTimeUs = mediaTimeUs;
       this.positionUs = positionUs;
     }
-
   }
 
   private final class PositionTrackerListener implements AudioTrackPositionTracker.Listener {
