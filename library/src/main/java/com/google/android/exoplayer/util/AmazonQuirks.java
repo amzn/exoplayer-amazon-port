1dd248cd9c37b94c38768dc38fa7ede20cee0e47
peddisri@amazon.com <peddisri@amazon.com>
2017-02-03 14:44:19 +0530

    FTVPI-1506: Enabled FOS4 latency workaround for Fire Phone

--- library/src/main/java/com/google/android/exoplayer/util/AmazonQuirks.java
+++ library/src/main/java/com/google/android/exoplayer/util/AmazonQuirks.java
@@ -30,6 +30,7 @@ public final class AmazonQuirks {
   private static final String FIRETV_STICK_DEVICE_MODEL = "AFTM";
   private static final String FIRETV_GEN2_DEVICE_MODEL = "AFTS";
   private static final String KINDLE_TABLET_DEVICE_MODEL = "KF";
+  private static final String FIRE_PHONE_DEVICE_MODEL = "SD";
   private static final String AMAZON = "Amazon";
   private static final String DEVICEMODEL = Build.MODEL;
   private static final String MANUFACTURER = Build.MANUFACTURER;
@@ -45,6 +46,7 @@ public final class AmazonQuirks {
   private static final boolean isFireTVGen2;
   private static final long fireTVFireOsBuildVersion;
   private static final boolean isKindleTablet;
+  private static final boolean isFirePhone;
 
   static {// This static block must be at the end
     //Init ordering is important within this block
@@ -53,6 +55,7 @@ public final class AmazonQuirks {
     isFireTVStick = isAmazonDevice && DEVICEMODEL.equalsIgnoreCase(FIRETV_STICK_DEVICE_MODEL);
     isFireTVGen2 = isAmazonDevice && DEVICEMODEL.equalsIgnoreCase(FIRETV_GEN2_DEVICE_MODEL);
     isKindleTablet = isAmazonDevice && DEVICEMODEL.startsWith(KINDLE_TABLET_DEVICE_MODEL);
+    isFirePhone = isAmazonDevice && DEVICEMODEL.startsWith(FIRE_PHONE_DEVICE_MODEL);
     fireTVFireOsBuildVersion = getBuildVersion();
   }
 
@@ -70,8 +73,8 @@ public final class AmazonQuirks {
   }
 
   public static boolean isLatencyQuirkEnabled() {
-    // Sets latency quirk for Amazon KK and JB Tablets
-    return ( (Util.SDK_INT <= 19) && isKindleTablet );
+    // Sets latency quirk for Amazon KK and JB Tablets and Phone
+    return ( (Util.SDK_INT <= 19) && (isKindleTablet || isFirePhone) );
   }
 
   public static int getAudioHWLatency() {
