e000b356e7b5be2bd79dca9152c4556b798112bb
peddisri@amazon.com <peddisri@amazon.com>
2017-03-10 16:06:31 +0530

    FTVPI-2081: Added support for Surround Sound detection for Optical out in FireOS based TVs

--- library/src/main/java/com/google/android/exoplayer/audio/AudioCapabilitiesReceiver.java
+++ library/src/main/java/com/google/android/exoplayer/audio/AudioCapabilitiesReceiver.java
@@ -18,11 +18,17 @@ package com.google.android.exoplayer.audio;
 import com.google.android.exoplayer.util.Assertions;
 import com.google.android.exoplayer.util.Util;
 
+import android.annotation.TargetApi;
 import android.content.BroadcastReceiver;
+import android.content.ContentResolver;
 import android.content.Context;
 import android.content.Intent;
 import android.content.IntentFilter;
+import android.database.ContentObserver;
 import android.media.AudioManager;
+import android.net.Uri;
+import android.os.Handler;
+import android.provider.Settings;
 
 /**
  * Notifies a listener when the audio playback capabilities change. Call {@link #register} to start
@@ -47,6 +53,10 @@ public final class AudioCapabilitiesReceiver {
   private final Context context;
   private final Listener listener;
   private final BroadcastReceiver receiver;
+  // AMZN_CHANGE_BEGIN
+  private final ContentResolver resolver;
+  private  SurroundSoundSettingObserver observer;
+  // AMZN_CHANGE_END
 
   /* package */ AudioCapabilities audioCapabilities;
 
@@ -60,6 +70,10 @@ public final class AudioCapabilitiesReceiver {
     this.context = Assertions.checkNotNull(context);
     this.listener = Assertions.checkNotNull(listener);
     this.receiver = Util.SDK_INT >= 21 ? new HdmiAudioPlugBroadcastReceiver() : null;
+    // AMZN_CHANGE_BEGIN
+    this.resolver = Util.SDK_INT >= 17 ? context.getContentResolver() : null;
+    this.observer = new SurroundSoundSettingObserver(null);
+    // AMZN_CHANGE_END
   }
 
   /**
@@ -73,7 +87,13 @@ public final class AudioCapabilitiesReceiver {
   public AudioCapabilities register() {
     Intent stickyIntent = receiver == null ? null
         : context.registerReceiver(receiver, new IntentFilter(AudioManager.ACTION_HDMI_AUDIO_PLUG));
-    audioCapabilities = AudioCapabilities.getCapabilities(stickyIntent);
+    // AMZN_CHANGE_BEGIN
+    audioCapabilities = AudioCapabilities.getCapabilities(context, stickyIntent);
+    Uri surroundSoundUri = Settings.Global.getUriFor(AudioCapabilities.EXTERNAL_SURROUND_SOUND_ENABLED);
+    if (resolver != null) {
+      resolver.registerContentObserver(surroundSoundUri, true, observer);
+    }
+    // AMZN_CHANGE_END
     return audioCapabilities;
   }
 
@@ -84,6 +104,11 @@ public final class AudioCapabilitiesReceiver {
     if (receiver != null) {
       context.unregisterReceiver(receiver);
     }
+    // AMZN_CHANGE_BEGIN
+    if (resolver != null) {
+      resolver.unregisterContentObserver(observer);
+    }
+    // AMZN_CHANGE_END
   }
 
   private final class HdmiAudioPlugBroadcastReceiver extends BroadcastReceiver {
@@ -91,7 +116,8 @@ public final class AudioCapabilitiesReceiver {
     @Override
     public void onReceive(Context context, Intent intent) {
       if (!isInitialStickyBroadcast()) {
-        AudioCapabilities newAudioCapabilities = AudioCapabilities.getCapabilities(intent);
+        // AMZN_CHANGE_ONELINE
+        AudioCapabilities newAudioCapabilities = AudioCapabilities.getCapabilities(context, intent);
         if (!newAudioCapabilities.equals(audioCapabilities)) {
           audioCapabilities = newAudioCapabilities;
           listener.onAudioCapabilitiesChanged(newAudioCapabilities);
@@ -100,5 +126,35 @@ public final class AudioCapabilitiesReceiver {
     }
 
   }
+  // AMZN_CHANGE_BEGIN
+  @TargetApi(17)
+  private final class SurroundSoundSettingObserver extends ContentObserver {
+
+    public SurroundSoundSettingObserver(Handler handler) {
+      super(handler);
+    }
+
+    @Override
+    public boolean deliverSelfNotifications() {
+      return true;
+    }
+
+    @Override
+    public void onChange(boolean selfChange) {
+      super.onChange(selfChange);
+      AudioCapabilities newAudioCapabilities;
+      int isSurroundSoundEnabled = Settings.Global.getInt(resolver,
+              AudioCapabilities.EXTERNAL_SURROUND_SOUND_ENABLED, 0);
+       if (isSurroundSoundEnabled == 1) {
+         newAudioCapabilities = AudioCapabilities.SURROUND_AUDIO_CAPABILITIES;
+       } else {
+         // fallback to last known audioCapabilities, hopefully updated by HDMI plugged state intent.
+         newAudioCapabilities = audioCapabilities;
+       }
+      listener.onAudioCapabilitiesChanged(newAudioCapabilities);
+    }
+
+  }
+  // AMZN_CHANGE_END
 
 }
