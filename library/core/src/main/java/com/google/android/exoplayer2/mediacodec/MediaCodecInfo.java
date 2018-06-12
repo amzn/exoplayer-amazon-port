8788471d424a08c8340fb22c8a2ad34a2e9a4548
Srikanth Peddibhotla <peddisri@amazon.com>
2018-06-12 14:13:12 +0530

    FIREFIVE-123614: Support skipping profile level checks for video
    [Problem]
    Some platforms support a profile / level of video decoder, but do not
    advertise in video decoder capabilities correctly.
    
    [Solution]
    If the app is confident that the profile and level is supported by a
    platform, and wants to avoid depending on its advertised capabilities,
    allow app to skip profile and level compatibility check before
    selecting and creating a video decoder

--- library/core/src/main/java/com/google/android/exoplayer2/mediacodec/MediaCodecInfo.java
+++ library/core/src/main/java/com/google/android/exoplayer2/mediacodec/MediaCodecInfo.java
@@ -25,6 +25,7 @@ import android.media.MediaCodecInfo.VideoCapabilities;
 import android.support.annotation.Nullable;
 import android.util.Pair;
 import com.google.android.exoplayer2.Format;
+import com.google.android.exoplayer2.util.AmazonQuirks;
 import com.google.android.exoplayer2.util.Assertions;
 import com.google.android.exoplayer2.util.Log;
 import com.google.android.exoplayer2.util.MimeTypes;
@@ -250,7 +251,8 @@ public final class MediaCodecInfo {
     }
     int profile = codecProfileAndLevel.first;
     int level = codecProfileAndLevel.second;
-    if (!isVideo && profile != CodecProfileLevel.AACObjectXHE) {
+    if (AmazonQuirks.shouldSkipProfileLevelCheck() || // AMZN_CHANGE_ONELINE
+            (!isVideo && profile != CodecProfileLevel.AACObjectXHE)) {
       // Some devices/builds under-report audio capabilities, so assume support except for xHE-AAC
       // which is not widely supported. See https://github.com/google/ExoPlayer/issues/5145.
       return true;
