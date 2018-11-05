30fbcb9849c820046fe65da94990d29f1ced58e0
Rinosh Sasidharan <rinoshs@amazon.com>
2018-11-05 21:26:22 +0530

    MMSW-5642: Add modified version string for Amazon Exoplayer port
    [Problem]
    Exoplayer version of Amazon Exoplayer is same upstream
    
    [Solution]
    Modify upstream version string to indicate that Amazon port

--- library/core/src/main/java/com/google/android/exoplayer2/ExoPlayerLibraryInfo.java
+++ library/core/src/main/java/com/google/android/exoplayer2/ExoPlayerLibraryInfo.java
@@ -31,9 +31,9 @@ public final class ExoPlayerLibraryInfo {
   // Intentionally hardcoded. Do not derive from other constants (e.g. VERSION_INT) or vice versa.
   public static final String VERSION = "2.11.3";
 
-  /** The version of the library expressed as {@code "ExoPlayerLib/" + VERSION}. */
+  /** The version of the library expressed as {@code "AmznExoPlayerLib/" + VERSION}. */
   // Intentionally hardcoded. Do not derive from other constants (e.g. VERSION) or vice versa.
-  public static final String VERSION_SLASHY = "ExoPlayerLib/2.11.3";
+  public static final String VERSION_SLASHY = "AmznExoPlayerLib/2.11.3";
 
   /**
    * The version of the library expressed as an integer, for example 1002003.
